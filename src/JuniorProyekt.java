174f62813ee52ff70dd17a07f330d300acd6d2d7
Hawsoo <timoguy99004@gmail.com>
2015-02-23 14:36:42 -0800

    Adds LWJGL3 (but not implemented)

--- src/JuniorProyekt.java
+++ src/JuniorProyekt.java
@@ -10,12 +10,14 @@ public class JuniorProyekt
 	public static void main(String[] argv)
 	{
 		// Parse and print args
+		System.out.println("Arguments:");
 		for (int i = 0; i < argv.length; i++)
 		{
 			System.out.println(argv[i]);
 		}
 		
 		// Start the game
+		System.out.println("\n=========== " + Game.name + " ===========\n");
 		new Game().launch();
 	}
 }
